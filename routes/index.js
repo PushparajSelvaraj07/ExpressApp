44f4784b1461a199d70d5425b71f1ad80f86a8a6
Pushparaj Selvaraj <Pushparaj.Selvaraj07@gmail.com>
2020-09-04 19:18:52 +0530

    push

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ExpressNew' });
+  res.render('index', { title: 'ExpressNewPush' });
 });
 
 module.exports = router;
