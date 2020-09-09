58a757998473c8bd53b49c073a571311f58fb27c
Pushparaj Selvaraj <Pushparaj.Selvaraj07@gmail.com>
2020-09-09 21:10:08 +0530

    good job

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ExpressSuccess' });
+  res.render('index', { title: 'ExpressSuccessGoodJobdone' });
 });
 
 module.exports = router;
